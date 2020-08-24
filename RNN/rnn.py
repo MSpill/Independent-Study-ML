a1d5e673a20e0d322e9baf51bc76f5673df18a8f
Matthew Spillman <matthewspillman@westminster.net>
2020-08-24 11:42:11 -0400

    Restructured code to allow for simpler multilayer RNNs

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -1,58 +1,46 @@
 import numpy as np  # for efficient matrix and vector operations
 
-# needs three vectors: input, state, and output
-# needs 2 bias vectors: state and output
-# needs three weight matrices: input to state (U), state to state (W), and state to output (V)
-
 
 class RNN:
 
-    def __init__(self, input_size, state_size, output_size):
-        # vectors
-        self.inputs = np.zeros((input_size, 1))
-        self.state = np.zeros((state_size, 1))
-        self.output = np.zeros((output_size, 1))
+    def __init__(self, input_size, state_sizes, output_size):
+        self.all_sizes = [input_size] + state_sizes + [output_size]
+        self.layer_sizes = state_sizes + [output_size]
+
+        # first in list will be inputs, last will be outputs, between is hidden state
+        self.values = [np.zeros((i, 1)) for i in self.all_sizes]
 
         # bias vectors
-        self.state_biases = np.random.normal(0, 0.01, (state_size, 1))
-        self.output_biases = np.random.normal(0, 0.01, (output_size, 1))
+        self.biases = [np.random.normal(
+            0, 0.01, (i, 1)) for i in self.layer_sizes]
 
         # weight matrices
-        self.input_to_state = np.random.normal(
-            0, 0.01, (state_size, input_size))
-        self.state_to_state = np.random.normal(
-            0, 0.01, (state_size, state_size))
-        self.state_to_output = np.random.normal(
-            0, 0.01, (output_size, state_size))
+        self.forward_weights = [np.random.normal(
+            0, 0.01, (self.all_sizes[i+1], self.all_sizes[i])) for i in range(0, self.all_sizes.__len__() - 1)]
+
+        self.recurrent_weights = [np.random.normal(
+            0, 0.01, (i, i)) for i in state_sizes]
 
     def set_input(self, input_vector):
-        self.inputs = input_vector
+        self.values[0] = input_vector
 
     def perform_timestep(self):
-        # calculate weighted sum
-        self.state = self.state_biases + \
-            np.dot(self.input_to_state, self.inputs) + \
-            np.dot(self.state_to_state, self.state)
-
-        # apply activation function
-        self.state = sigmoid(self.state)
-
-    def calculate_output(self):
-        # calculate weighted sum
-        self.output = self.output_biases + \
-            np.dot(self.state_to_output, self.state)
-
-        # apply activation function
-        self.output = sigmoid(self.output)
+        for i in range(1, self.all_sizes.__len__()):
+            # calculate weighted sum in from previous layer or input
+            new_vals = self.biases[i-1] + \
+                np.dot(self.forward_weights[i-1], self.values[i-1])
+            # if this is a hidden layer, add the recurrent signal from its previous state
+            if i < len(self.layer_sizes)-1:
+                new_vals += np.dot(self.recurrent_weights[i-1], self.values[i])
+            # apply activation function
+            self.values[i] = sigmoid(new_vals)
 
 
 def sigmoid(x):
     return 1 / (1 + 2**(-x))
 
 
-myRnn = RNN(10, 5, 7)
-myRnn.perform_timestep()
-myRnn.calculate_output()
-print(myRnn.state_to_state)
-print(myRnn.state)
-print(myRnn.output)
+if __name__ == '__main__':
+    myRnn = RNN(10, [5, 4], 7)
+    myRnn.perform_timestep()
+    print(myRnn.values)
